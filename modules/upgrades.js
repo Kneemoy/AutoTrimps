c52f590d35236322aea90bffcfaab3ad696004ad
MKKubik <matthewkubik@outlook.com>
2018-04-14 23:18:19 -0500

    updated equals and not equals operators (==, !=) to new format (===, !==) to prevent unexpected type coercion vars updated to const to maintain block scope and prevent hoisting to global var updated for in loop to for of loop changed formatting to more closely match javascript standard style gui more comments!

--- modules/upgrades.js
+++ modules/upgrades.js
@@ -1,25 +1,53 @@
 //MODULES["upgrades"] = {};
-var upgradeList = ['Miners', 'Scientists', 'Coordination', 'Speedminer', 'Speedlumber', 'Speedfarming', 'Speedscience', 'Speedexplorer', 'Megaminer', 'Megalumber', 'Megafarming', 'Megascience', 'Efficiency', 'TrainTacular', 'Trainers', 'Explorers', 'Blockmaster', 'Battle', 'Bloodlust', 'Bounty', 'Egg', 'Anger', 'Formations', 'Dominance', 'Barrier', 'UberHut', 'UberHouse', 'UberMansion', 'UberHotel', 'UberResort', 'Trapstorm', 'Gigastation', 'Shieldblock', 'Potency', 'Magmamancers'];
+const upgradeList = ['Miners', 'Scientists', 'Coordination', 'Speedminer', 'Speedlumber', 'Speedfarming', 'Speedscience', 'Speedexplorer',
+  'Megaminer', 'Megalumber', 'Megafarming', 'Megascience', 'Efficiency', 'TrainTacular', 'Trainers', 'Explorers', 'Blockmaster', 'Battle',
+  'Bloodlust', 'Bounty', 'Egg', 'Anger', 'Formations', 'Dominance', 'Barrier', 'UberHut', 'UberHouse', 'UberMansion', 'UberHotel',
+  'UberResort', 'Trapstorm', 'Gigastation', 'Shieldblock', 'Potency', 'Magmamancers'];
 
 //Buys all available non-equip upgrades listed in var upgradeList
-function buyUpgrades() {
-    for (var upgrade in upgradeList) {
-        upgrade = upgradeList[upgrade];
-        var gameUpgrade = game.upgrades[upgrade];
-        var available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
-        if (upgrade == 'Coordination' && (getPageSetting('ManualCoords') || !canAffordCoordinationTrimps())) continue;
-        if (upgrade == 'Shieldblock' && !getPageSetting('BuyShieldblock')) continue;
-        if (upgrade == 'Gigastation' && (game.global.lastWarp ? game.buildings.Warpstation.owned < (Math.floor(game.upgrades.Gigastation.done * getPageSetting('DeltaGigastation')) + getPageSetting('FirstGigastation')) : game.buildings.Warpstation.owned < getPageSetting('FirstGigastation'))) continue;
-        //skip bloodlust during scientist challenges and while we have autofight enabled.
-        if (upgrade == 'Bloodlust' && game.global.challengeActive == 'Scientist' && getPageSetting('BetterAutoFight')) continue;
-        //skip potency when autoBreedTimer is disabled
-        if (upgrade == 'Potency' && getPageSetting('GeneticistTimer') >= 0) continue;
+function buyUpgrades () {
+  for (const upgrade of upgradeList) {
+    const gameUpgrade = game.upgrades[upgrade];
+    const available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
 
-        //Main logics:
-        if (!available) continue;
-        if (game.upgrades.Scientists.done < game.upgrades.Scientists.allowed && upgrade != 'Scientists') continue;
-        buyUpgrade(upgrade, true, true);
-        debug('Upgraded ' + upgrade, "upgrades", "*upload2");
-    //loop again.
+    //skip coordination if its set to manual, or it cant be afforded
+    if (upgrade === 'Coordination' && (getPageSetting('ManualCoords') || !canAffordCoordinationTrimps())) {
+      continue;
     }
+
+    //skip shieldblock if its setting is disabled
+    if (upgrade === 'Shieldblock' && !getPageSetting('BuyShieldblock')) {
+      continue;
+    }
+
+    //skip gigastation if its disabled, or if next gigastation doesnt fall in line with your warpstation + delta settings
+    if (upgrade === 'Gigastation'
+      && (game.global.lastWarp
+        ? game.buildings.Warpstation.owned < (Math.floor(game.upgrades.Gigastation.done * getPageSetting('DeltaGigastation')) + getPageSetting('FirstGigastation'))
+        : game.buildings.Warpstation.owned < getPageSetting('FirstGigastation'))) {
+      continue;
+    }
+
+    //skip bloodlust during scientist challenges and while we have autofight enabled.
+    if (upgrade === 'Bloodlust' && game.global.challengeActive === 'Scientist' && getPageSetting('BetterAutoFight')) {
+      continue;
+    }
+
+    //skip potency when autoBreedTimer is disabled
+    if (upgrade === 'Potency' && getPageSetting('GeneticistTimer') >= 0) {
+      continue;
+    }
+
+    //Main logics:
+    if (!available) {
+      continue;
+    }
+    if (game.upgrades.Scientists.done < game.upgrades.Scientists.allowed && upgrade !== 'Scientists') {
+      continue;
+    }
+
+    buyUpgrade(upgrade, true, true);
+    debug('Upgraded ' + upgrade, 'upgrades', '*upload2');
+    //loop again.
+  }
 }
